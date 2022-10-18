60762059ccd36955804305a46c39b6ae3b657824
Yury Boichuk <slon@offline.lv>
2022-10-18 12:52:24 +0300

    custom exception for TelegramBotApi

--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,17 +2,30 @@
 
 namespace App\Services\Telegram;
 
+use App\Exceptions\TelegramMessageNotSentException;
+use Illuminate\Http\Client\RequestException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
     public const HOST = 'https://api.telegram.org/bot';
 
-    public static function sendMessage(string $token, int $chatId, string $text): void
+    /**
+     * @throws TelegramMessageNotSentException
+     */
+    public static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        Http::get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text,
-        ]);
+        # TODO: получать json ответ и возвращать в методе boolean
+        # TODO: добавить try catch и кастомный exception
+        try {
+            Http::get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text,
+            ])->throw()->json();
+        } catch (RequestException $e) {
+            throw new TelegramMessageNotSentException($e->getMessage(), $e->getCode());
+        }
+
+        return true;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    logger()->channel('telegram')->debug('hey!');
     return view('welcome');
 });
