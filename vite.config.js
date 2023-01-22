9df1d032c6043b7f5315a9e280a6216867df84b9
Yury Boichuk <slon@offline.lv>
2023-01-22 09:01:47 +0300

    vite ssl config fix

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,29 @@
-import {defineConfig} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import mkcert from 'vite-plugin-mkcert';
+import {defineConfig} from 'vite'
+import laravel from 'laravel-vite-plugin'
+import fs from 'fs';
+import {resolve} from 'path';
+import {homedir} from 'os';
+
+let host = 'shop.test'
+let homeDirectory = homedir();
+
+let serverConfig;
+serverConfig = {
+    https: {
+        key: fs.readFileSync(resolve(homeDirectory, `.config/valet/Certificates/${host}.key`)),
+        cert: fs.readFileSync(resolve(homeDirectory, `.config/valet/Certificates/${host}.crt`)),
+    },
+    hmr: {
+        host: host,
+    },
+    host: host,
+};
 
 export default defineConfig({
+    server: serverConfig,
     plugins: [
-        mkcert(),
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/sass/main.sass',
-                'resources/js/app.js'
-            ],
-            refresh: true,
-        }),
+        laravel([
+            'resources/js/app.js',
+        ]),
     ],
-});
+})
